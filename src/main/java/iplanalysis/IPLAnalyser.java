b7e2f7063978352ba030ebf2754199365f00d7e9
kumudgarg <kumud50garg@gmail.com>
2019-12-14 15:52:42 +0530

    REFACTORED : code to IPLAnalyser by replacing while loop with streamSupport stream functionality

--- src/main/java/iplanalysis/IPLAnalyser.java
+++ src/main/java/iplanalysis/IPLAnalyser.java
@@ -3,31 +3,28 @@ package iplanalysis;
 import csvbulider_jar.CSVBuilderException;
 import csvbulider_jar.CSVBuilderFactory;
 import csvbulider_jar.ICSVBuilder;
-
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Iterator;
+import java.util.stream.StreamSupport;
 
 public class IPLAnalyser {
+
     public int loadIPLMostRunsData(String csvFilePath) throws IPLCSVException {
-        int count = -1;
         try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))) {
             ICSVBuilder csvBuilder = CSVBuilderFactory.createCSVBuilder();
             Iterator<MostRunCSV> mostRunCSVIterator = csvBuilder.getCSVFileIterator(reader, MostRunCSV.class);
-            while (mostRunCSVIterator.hasNext()) {
-                mostRunCSVIterator.next();
-                count++;
-            }
-            return count;
+            Iterable<MostRunCSV> mostRunCSVS = () -> mostRunCSVIterator;
+            long ipLRecords = StreamSupport.stream(mostRunCSVS.spliterator(), false).count();
+            return (int) ipLRecords;
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new IPLCSVException(e.getMessage(), IPLCSVException.ExceptionType.NO_CENSUS_DATA);
         } catch (CSVBuilderException e) {
             throw new IPLCSVException(e.getMessage(), IPLCSVException.ExceptionType.CENSUS_FILE_PROBLEM);
         } catch (RuntimeException e) {
             throw new IPLCSVException(e.getMessage(), IPLCSVException.ExceptionType.CSV_FILE_INTERNAL_ISSUES);
         }
-        return 0;
     }
 }
