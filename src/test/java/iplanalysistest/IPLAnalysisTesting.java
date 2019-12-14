8804109b0d706fd3253f96362f20768de8c776b6
kumudgarg <kumud50garg@gmail.com>
2019-12-14 15:15:14 +0530

    Test : if wrong csv file given then would it be given custom exception type or not

--- src/test/java/iplanalysistest/IPLAnalysisTesting.java
+++ src/test/java/iplanalysistest/IPLAnalysisTesting.java
@@ -1,15 +1,34 @@
 package iplanalysistest;
 
 import iplanalysis.IPLAnalyser;
+import iplanalysis.IPLCSVException;
 import org.junit.Assert;
 import org.junit.Test;
+
 public class IPLAnalysisTesting {
     private static final String IPL_MOST_RUNS_CSV_FILE_PATH = "./src/test/resources/IPL2019FactsheetMostRuns.csv";
+    private static final String IPL_Wrong_CSV_FILE_PATH = "./src/test/resources/IPL2019MostRuns.csv";
 
     @Test
     public void givenIPLMOstRunsCSVFile_ShouldReturnCorrectRecords() {
-        IPLAnalyser iplAnalyser = new IPLAnalyser();
-        int i = iplAnalyser.loadIPLMostRunsData(IPL_MOST_RUNS_CSV_FILE_PATH);
-        Assert.assertEquals(100,i);
+        try {
+            IPLAnalyser iplAnalyser = new IPLAnalyser();
+            int iplRecords = iplAnalyser.loadIPLMostRunsData(IPL_MOST_RUNS_CSV_FILE_PATH);
+            Assert.assertEquals(100, iplRecords);
+        } catch (IPLCSVException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    public void givenWrongIPLMOstRunsCSVFile_ShouldReturnCustomExceptionType() {
+        try {
+            IPLAnalyser iplAnalyser = new IPLAnalyser();
+            iplAnalyser.loadIPLMostRunsData(IPL_Wrong_CSV_FILE_PATH);
+        } catch (IPLCSVException e) {
+           Assert.assertEquals(IPLCSVException.ExceptionType.CSV_FILE_INTERNAL_ISSUES, e.type);
+        }
+
     }
 }
