8d105a821338166bf616fbf6314ab32869449145
kumudgarg <kumud50garg@gmail.com>
2019-12-14 15:36:55 +0530

    Test : if headers are Missed from csv file given then would it be given custom exception type or not

--- src/test/java/iplanalysistest/IPLAnalysisTesting.java
+++ src/test/java/iplanalysistest/IPLAnalysisTesting.java
@@ -9,6 +9,7 @@ public class IPLAnalysisTesting {
     private static final String IPL_MOST_RUNS_CSV_FILE_PATH = "./src/test/resources/IPL2019FactsheetMostRuns.csv";
     private static final String IPL_Wrong_CSV_FILE_PATH = "./src/test/resources/IPL2019MostRuns.csv";
     private static final String Wrong_Delimiter_CSV_FILE = "./src/test/resources/WrongDelimiterCSV.csv";
+    private static final String Missing_Header_CSV_File = "./src/test/resources/MissingHeaderCSV.csv";
 
     @Test
     public void givenIPLMOstRunsCSVFile_ShouldReturnCorrectRecords() {
@@ -38,6 +39,17 @@ public class IPLAnalysisTesting {
         try {
             IPLAnalyser iplAnalyser = new IPLAnalyser();
             iplAnalyser.loadIPLMostRunsData(Wrong_Delimiter_CSV_FILE);
+        } catch (IPLCSVException e) {
+            Assert.assertEquals(IPLCSVException.ExceptionType.CSV_FILE_INTERNAL_ISSUES, e.type);
+        }
+
+    }
+
+    @Test
+    public void givenMissingHeaderIPLMOstRunsCSVFile_ShouldReturnCustomExceptionType() {
+        try {
+            IPLAnalyser iplAnalyser = new IPLAnalyser();
+            iplAnalyser.loadIPLMostRunsData(Missing_Header_CSV_File);
         } catch (IPLCSVException e) {
             Assert.assertEquals(IPLCSVException.ExceptionType.CSV_FILE_INTERNAL_ISSUES, e.type);
         }
